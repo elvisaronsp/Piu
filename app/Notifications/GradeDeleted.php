76dde2808fc801756da2df03ea9f1e7158f1b187
Junior Santos <jsantos.class@gmail.com>
2019-02-14 14:47:00 -0300

    Update GradeDeleted.php

--- app/Notifications/GradeDeleted.php
+++ app/Notifications/GradeDeleted.php
@@ -43,10 +43,11 @@ class GradeDeleted extends Notification
     public function toMail($notifiable)
     {
       $employeer = $this->grade->employeer_id;
+      $employeer = $employeer == 0? 'A instituição' : $this->grade->employeer->name;
       return (new MailMessage)
              ->greeting('Olá,')
              ->line("A nota do aluno {$this->grade->student_group->student->name} foi excluída.")
-             ->line("O funcionário que realizou a exclusão foi {$employeer == 0? 'A instituição':$this->grade->employeer->name }.")
+             ->line("O funcionário que realizou a exclusão foi {$employeer}.")
              //->action('View Invoice', $url)
              ->line('Att');
     }
